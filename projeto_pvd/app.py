591b9d4ac72df74daecc332b9dcb35fc9e7c4877
Rafael Lucas <rafaellucas-ti@hotmail.com>
2025-02-06 16:05:14 -0300

    Sistema de votação

--- projeto_pvd/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, jsonify
-
-app= Flask(__name__)
-
-@app.route('/products', methods=['GET, POST'])
-def products():
-    if request.method == 'POST':
-        data = request.json
